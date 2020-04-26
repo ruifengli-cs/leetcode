24d7162bbab9c6ccdccce37ea60f6a47b3dceced
Ruifeng Li <li.rui.ce@gmail.com>
2020-04-26 13:15:30 -0400

    Update lintcode140. Fast Power-R1.py

--- Binary Search/lintcode140. Fast Power-R1.py
+++ Binary Search/lintcode140. Fast Power-R1.py
@@ -5,43 +5,45 @@ class Solution:
     @param n: A 32bit integer
     @return: An integer
     """
-# a ** n = a * a * a * a * a * ....* a (n times)= a^2 * a^2 *..*a^2(n/2 times)
-# a ** n = (a ** n/2) ^ 2 
-# APP1: recursion
+# APP1: brute force: multiply one by one recursively
+Time: O(n) space: O(1) stack depth = O(n) Runtime: maximum recursion depth exceeded
     def fastPower(self, a, b, n):
-        if n < 0:
-            a = 1 / a 
         if n == 0:
             return 1 % b
-        ans = self.dfs(a, b, n)
-        return ans
+        return (a * self.fastPower(a, b, n - 1)) % b
     
-    def dfs(self, a, b, n):
+# APP2: brute force: multiply one by one iteratively
+# Time: O(n) space: O(1) Runtime: TLE
+    def fastPower(self, a, b, n):
+        # bug need to check if n == 0
         if n == 0:
-            return 1 
-        if n % 2 != 0:
-            return self.dfs(a, b, n // 2) ** 2 * a % b
-        return self.dfs(a, b, n // 2) ** 2 % b
-
-# APP1: another implementation
+            return 1 % b
+        power = 1
+        for i in range(n):
+            power = (power * a) % b
+        return power
+    
+# APP3: binary search multiply recursively
+# Time: O(lgn) space: O(1)
     def fastPower(self, a, b, n):
         if n == 0:
             return 1 % b
         power = self.fastPower(a, b, n // 2)
-        ans = power * power % b
+        ans = power * power
         if n % 2 != 0:
-            ans = ans * a % b
-        return ans    
-        
-# APP2: iterative
+            ans = (ans * a) % b
+        return ans % b
+
+# APP4: binary search iterative approach to multiply exponentially
+# Time: O(lgn) space: O(1)
     def fastPower(self, a, b, n):
         if n == 0:
-            return 1 % b 
-        base = a 
-        ans = 1 
+            return 1 % b
+        ans = 1
         while n > 0:
-            if n % 2 != 0:
-                ans = ans * base % b
-            n = n // 2 
-            base = base * base 
-        return ans
+            if n % 2 == 1:
+                ans *= a
+            a = a * a % b
+            n = n // 2
+        return ans % b
+        
