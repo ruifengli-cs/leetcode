44629da93b9e31868735676d3f5325a194b2bc1c
Ryan Li <ruifengl@asu.edu>
2020-05-19 20:06:26 -0400

    grumpy bookstore

--- Two pointers/1052. Grumpy Bookstore Owner.py
+++ Two pointers/1052. Grumpy Bookstore Owner.py
@@ -1,47 +1,43 @@
 class Solution:
-# APP1: one pass to find X continous unsatisfied, one pass to count max happy
-# Time; O(n) space: O(1) Runtime: 84, memory: 100%
+    # APP1: brute force: loop through starting position for x.
+    # time: O(nX) Space: O(1) Runtime: TLE
     def maxSatisfied(self, customers: List[int], grumpy: List[int], X: int) -> int:
         if not customers or not grumpy:
             return 0
-        max_not_happy = 0
-        total_happy = 0
-        n = len(customers)
-        left = right = 0
-        while right < n and right < X:
-            if grumpy[right] == 1:
-                max_not_happy += customers[right]
-            right += 1
-        new_not_happy = max_not_happy
-        while right < n:
-            if grumpy[right] == 1:
-                new_not_happy += customers[right]
-            if grumpy[left] == 1:
-                new_not_happy -= customers[left]
-            max_not_happy = max(max_not_happy, new_not_happy)
-            right += 1
-            left += 1
+        #       find total satified customers when grumpy is 0
+        total_not_grumpy, n = 0, len(customers)
         for i in range(n):
-            if grumpy[i] == 0:
-                total_happy += customers[i]
-        return total_happy + max_not_happy
-        
-# APP2: sliding window one pass to find both for APP2
-# Time; O(n) space: O(1) Runtime: 99%, memory: 100%
+            if not grumpy[i]:
+                total_not_grumpy += customers[i]
+        #       find max satisfied customers when grumpy
+        max_grumpy = 0
+        for i in range(n):
+            now_grumpy = 0
+            for j in range(X):
+                new_idx = i + j
+                if new_idx < n and grumpy[new_idx]:
+                    now_grumpy += customers[new_idx]
+            max_grumpy = max(max_grumpy, now_grumpy)
+        return total_not_grumpy + max_grumpy
+
+    # APP2: sliding window to find happy customers when grumpy
     def maxSatisfied(self, customers: List[int], grumpy: List[int], X: int) -> int:
         if not customers or not grumpy:
             return 0
-        n = len(grumpy)
-        total_happy = max_not_happy = now_not_happy = left = 0
-        
-        for right in range(n):
-            if grumpy[right] == 0:
-                total_happy += customers[right]
+
+        total_not_grumpy, n = 0, len(customers)
+        max_grumpy = l = now_grumpy = 0
+        for r in range(n):
+            #       find total satified customers when not grumpy
+            if not grumpy[r]:
+                total_not_grumpy += customers[r]
                 continue
-            now_not_happy += customers[right]
-            while right - left >= X:
-                if grumpy[left] == 1:
-                    now_not_happy -= customers[left]
-                left += 1
-            max_not_happy = max(max_not_happy, now_not_happy)
-        return max_not_happy + total_happy
+
+            #       find total satified customers when grumpy
+            now_grumpy += customers[r]
+            while r - l + 1 > X:
+                if grumpy[l]:
+                    now_grumpy -= customers[l]
+                l += 1
+            max_grumpy = max(max_grumpy, now_grumpy)
+        return total_not_grumpy + max_grumpy
