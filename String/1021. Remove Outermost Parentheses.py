9d25e5e0e8a7a65c0107e29e4d71fbe188f9faf3
Ryan Li <ruifengl@asu.edu>
2020-06-10 16:49:38 -0400

    Update 1021. Remove Outermost Parentheses.py

--- String/1021. Remove Outermost Parentheses.py
+++ String/1021. Remove Outermost Parentheses.py
@@ -1,13 +1,46 @@
 class Solution:
+    # APP1: count open_left
     def removeOuterParentheses(self, S: str) -> str:
         if not S:
-            return ''
-        opened = 0
-        res = []
+            return ""
+        open_left = 0
+        n, res = len(S), []
+        for i in range(n):
+            if S[i] == '(':
+                if open_left > 0:
+                    res.append('(')
+                open_left += 1
+            if S[i] == ')':
+                if open_left > 1:
+                    res.append(')')
+                open_left -= 1
+        return ''.join(res)
+
+    # APP2: concise version of APP1:
+    def removeOuterParentheses(self, S: str) -> str:
+        res, open_left = [], 0
         for ch in S:
-            if ch == '(' and opened > 0:
+            if ch == '(' and open_left > 0:
                 res.append(ch)
-            if ch == ')' and opened > 1:
+            if ch == ')' and open_left > 1:
                 res.append(ch)
-            opened += 1 if ch == '(' else -1
-        return "".join(res)
+            open_left += 1 if ch == '(' else -1
+        return ''.join(res)
+
+    # APP3: two pointers, append string without outer () when open_left == 0
+    def removeOuterParentheses(self, S: str) -> str:
+        res, open_left, l = [], 0, 0
+        for r in range(len(S)):
+            if S[r] == '(':
+                open_left += 1
+            if S[r] == ')':
+                open_left -= 1
+            if open_left == 0:
+                res.append(S[l + 1: r])
+                l = r + 1
+        return ''.join(res)
+
+
+
+
+
